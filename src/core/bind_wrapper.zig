6f57c562baacc2160f4ce53ae05d1cf1127ae149
thimenesup <thimenesup@gmail.com>
2024-08-21 19:40:56 +0200

    Improve bind wrapper calls to handle both by value and pointer arguments

--- src/core/bind_wrapper.zig
+++ src/core/bind_wrapper.zig
@@ -5,9 +5,38 @@ const gd = @import("../godot.zig");
 
 const Variant = @import("../core/variant.zig").Variant;
 
-inline fn ptrCallFunction(comptime function: anytype, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
-    const fn_info = @typeInfo(@TypeOf(function)).Fn;
+inline fn ArgBaseType(comptime T: type) type {
+    const type_info = @typeInfo(T);
+    const type_tag = @typeInfo(std.builtin.Type).Union.tag_type.?;
+    switch (type_info) {
+        type_tag.Pointer => {
+            return type_info.Pointer.child;
+        },
+        else => {
+            return T;
+        }
+    }
+}
 
+inline fn isPointerType(comptime T: type) bool {
+    const type_info = @typeInfo(T);
+    const type_tag = @typeInfo(std.builtin.Type).Union.tag_type.?;
+    switch (type_info) {
+        type_tag.Pointer => {
+            return true;
+        },
+        else => {
+            return false;
+        }
+    }
+}
+
+
+inline fn ptrCall(comptime class: type, comptime function: anytype, comptime is_method: bool, instance: gi.GDExtensionClassInstancePtr, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
+    const fn_info = @typeInfo(@TypeOf(function)).Fn;
+    const struct_instance: ?*class = @ptrCast(@alignCast(instance));
+
+    // Pass call arguments as either dereferenced value or the pointer itself, matching the signature
     comptime var arg_types: [fn_info.params.len]type = undefined;
     inline for (fn_info.params, 0..) |param, i| {
         arg_types[i] = param.type.?;
@@ -16,27 +45,73 @@ inline fn ptrCallFunction(comptime function: anytype, args: [*c]const gi.GDExten
     const TupleType = std.meta.Tuple(&arg_types);
     var arg_tuple: TupleType = undefined;
     inline for (fn_info.params, 0..) |param, i| {
-        arg_tuple[i] = @as(*const param.type.?, @alignCast(@ptrCast(args[i]))).*;
+        if (is_method and i == 0) {
+            arg_tuple[i] = @ptrCast(struct_instance);
+        } else {
+            const arg_index = if (is_method) i - 1 else i; // Minus 1 to offset struct
+            const base_type = ArgBaseType(param.type.?);
+            const arg_typed_ptr: *base_type = @alignCast(@constCast(@ptrCast(args[arg_index])));
+            if (isPointerType(param.type.?)) {
+                arg_tuple[i] = arg_typed_ptr;
+            } else {
+                arg_tuple[i] = arg_typed_ptr.*;
+            }
+        }
     }
 
     const result = @call(.auto, function, arg_tuple);
-    if (r_return != null) {
-        @as(*fn_info.return_type.?, @alignCast(@ptrCast(r_return))).* = result;
+    const r_variant: ?*Variant = @ptrCast(r_return);
+    if (r_variant != null) {
+        r_variant.?.* = Variant.typeAsVariant(fn_info.return_type.?)(&result);
     }
+}
+
+inline fn ptrCallFunction(comptime function: anytype, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
+    ptrCall(void, function, false, null, args, r_return);
 }
 
 inline fn ptrCallMethod(comptime class: type, comptime function: anytype, instance: gi.GDExtensionClassInstancePtr, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
     const fn_info = @typeInfo(@TypeOf(function)).Fn;
-    const struct_instance: *class = @ptrCast(@alignCast(instance));
-
     comptime if (fn_info.params.len == 0) {
         @compileError("A method needs to take atleast the struct parameter");
     };
-
     comptime if (fn_info.params[0].type.? != *class and fn_info.params[0].type.? != *const class) {
         @compileError("The first parameter of a method should be the struct");
     };
 
+    ptrCall(class, function, true, instance, args, r_return);
+}
+
+
+inline fn variantCall(comptime class: type, comptime function: anytype, comptime is_method: bool, instance: gi.GDExtensionClassInstancePtr, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
+    const fn_info = @typeInfo(@TypeOf(function)).Fn;
+    const struct_instance: ?*class = @ptrCast(@alignCast(instance));
+
+    // Need to store the value of every converted variant
+    comptime var arg_base_types: [fn_info.params.len]type = undefined;
+    inline for (fn_info.params, 0..) |param, i| {
+        if (is_method and i == 0) {
+            arg_base_types[i] = param.type.?;
+        } else {
+            arg_base_types[i] = ArgBaseType(param.type.?);
+        }
+    }
+
+    const StorageTupleType = std.meta.Tuple(&arg_base_types);
+    var arg_storage: StorageTupleType = undefined;
+    inline for (arg_base_types, 0..) |arg_base_type, i| {
+        if (is_method and i == 0) {
+            continue;
+        } else {
+            const arg_index = if (is_method) i - 1 else i; // Minus 1 to offset struct
+            const conversion_fn = Variant.variantAsType(arg_base_type);
+            arg_storage[i] = conversion_fn(@ptrCast(args[arg_index]));
+        }
+    }
+
+    // Pass call arguments as either the stored value or a pointer to it, matching the signature
+    // NOTE: C++ GDExtension won't compile if using non const ptrs in variant calls
+    // The reason is likely that since the variant gets converted to a copy, the passed value won't get actually mutated, which can lead to confusion, but we will allow it
     comptime var arg_types: [fn_info.params.len]type = undefined;
     inline for (fn_info.params, 0..) |param, i| {
         arg_types[i] = param.type.?;
@@ -45,72 +120,38 @@ inline fn ptrCallMethod(comptime class: type, comptime function: anytype, instan
     const TupleType = std.meta.Tuple(&arg_types);
     var arg_tuple: TupleType = undefined;
     inline for (fn_info.params, 0..) |param, i| {
-        if (i == 0) {
-            arg_tuple[i] = struct_instance;
+        if (is_method and i == 0) {
+            arg_tuple[i] = @ptrCast(struct_instance);
         } else {
-            arg_tuple[i] = @as(*const param.type.?, @alignCast(@ptrCast(args[i - 1]))).*; // Minus 1 to offset struct
+            if (isPointerType(param.type.?)) {
+                arg_tuple[i] = &arg_storage[i];
+            } else {
+                arg_tuple[i] = arg_storage[i];
+            }
         }
     }
 
     const result = @call(.auto, function, arg_tuple);
-    if (r_return != null) {
-        @as(*fn_info.return_type.?, @alignCast(@ptrCast(r_return))).* = result;
+    const r_variant: ?*Variant = @ptrCast(r_return);
+    if (r_variant != null) {
+        r_variant.?.* = Variant.typeAsVariant(fn_info.return_type.?)(&result);
     }
 }
 
 inline fn variantCallFunction(comptime function: anytype, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
-    const fn_info = @typeInfo(@TypeOf(function)).Fn;
-
-    comptime var arg_types: [fn_info.params.len]type = undefined;
-    inline for (fn_info.params, 0..) |param, i| {
-        arg_types[i] = param.type.?;
-    }
-
-    const TupleType = std.meta.Tuple(&arg_types);
-    var arg_tuple: TupleType = undefined;
-    inline for (fn_info.params, 0..) |param, i| {
-        arg_tuple[i] = Variant.variantAsType(param.type.?)(@ptrCast(args[i]));
-    }
-
-    const result = @call(.auto, function, arg_tuple);
-    const r_variant: ?*Variant = @ptrCast(r_return);
-    if (r_variant != null) {
-        r_variant.?.* = Variant.typeAsVariant(fn_info.return_type.?)(&result);
-    }
+    variantCall(void, function, false, null, args, r_return);
 }
 
 inline fn variantCallMethod(comptime class: type, comptime function: anytype, instance: gi.GDExtensionClassInstancePtr, args: [*c]const gi.GDExtensionConstTypePtr, r_return: gi.GDExtensionTypePtr) void {
     const fn_info = @typeInfo(@TypeOf(function)).Fn;
-    const struct_instance: *class = @ptrCast(@alignCast(instance));
-
     comptime if (fn_info.params.len == 0) {
         @compileError("A method needs to take atleast the struct parameter");
     };
-
     comptime if (fn_info.params[0].type.? != *class and fn_info.params[0].type.? != *const class) {
         @compileError("The first parameter of a method should be the struct");
     };
 
-    comptime var arg_types: [fn_info.params.len]type = undefined;
-    inline for (fn_info.params, 0..) |param, i| {
-        arg_types[i] = param.type.?;
-    }
-
-    const TupleType = std.meta.Tuple(&arg_types);
-    var arg_tuple: TupleType = undefined;
-    inline for (fn_info.params, 0..) |param, i| {
-        if (i == 0) {
-            arg_tuple[i] = struct_instance;
-        } else {
-            arg_tuple[i] = Variant.variantAsType(param.type.?)(@ptrCast(args[i - 1])); // Minus 1 to offset struct
-        }
-    }
-
-    const result = @call(.auto, function, arg_tuple);
-    const r_variant: ?*Variant = @ptrCast(r_return);
-    if (r_variant != null) {
-        r_variant.?.* = Variant.typeAsVariant(fn_info.return_type.?)(&result);
-    }
+    variantCall(class, function, true, instance, args, r_return);
 }
 
 
@@ -135,7 +176,8 @@ inline fn variantCallValidate(comptime function: anytype, comptime is_method: bo
         const variant_arg_index = if (is_method) i - 1 else i;
 
         const variant_arg: *const Variant = @ptrCast(args[variant_arg_index]);
-        const expected_variant_type = Variant.typeToVariantType(param.type.?);
+        const param_base_type = ArgBaseType(param.type.?);
+        const expected_variant_type = Variant.typeToVariantType(param_base_type);
         const gi_variant_type: gi.GDExtensionVariantType = @enumFromInt(@intFromEnum(variant_arg.getType()));
         const gi_expected_type: gi.GDExtensionVariantType = @enumFromInt(@intFromEnum(expected_variant_type));
         if (!gd.interface.?.variant_can_convert_strict.?(gi_variant_type, gi_expected_type)) {
